79d41e8ca550f104a14948fc3f2ae6c06a8c5a65
Jordan A Holt <jordanh316@gmail.com>
2025-05-23 01:30:27 -0500

    Remove ToastContainer from ToastProvider render method

--- pdlpropmanager_frontend/src/app/components/ui/Toast/ToastProvider.jsx
+++ pdlpropmanager_frontend/src/app/components/ui/Toast/ToastProvider.jsx
@@ -17,7 +17,7 @@ export const ToastProvider = ({ children }) => {
 
   return (
     <ToastContext.Provider value={{ showToast }}>
-      <ToastContainer />
+      
       {children}
       <div className="fixed top-4 right-4 z-50 space-y-2">
         <AnimatePresence>
